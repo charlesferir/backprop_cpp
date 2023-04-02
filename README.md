ccf4c113c241f4eaaa2596c6c42be57d416ab4f2
Charles Ferir <charlesferir1@gmail.com>
2023-04-02 17:22:59 +0200

    Added arch profile and Network class

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# backprop_cpp
+# A backprop implementation for 'fun'!
+
+Linux package install with `conan`:
+```
+conan install . -pr:h=conan/profiles/x86_64_linux -pr:b=conan/profiles/x86_64_linux -s build_type=Release --build=missing -of=build/x86_64_linux/release
+conan install . -pr:h=conan/profiles/x86_64_linux -pr:b=conan/profiles/x86_64_linux -s build_type=Debug --build=missing -of=build/x86_64_linux/debug
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
+#include <iostream>
+#include <memory>
+#include <random>
+#include <string>
+
 #include <Eigen/Eigen>
 #include <cxxopts.hpp>
-#include <iostream>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 auto console = spdlog::stdout_color_mt("main.cpp");
+
+long unsigned int seed = 666;
+std::default_random_engine generator{seed};
+std::normal_distribution<double> distribution(0.0, 1.0);
+
+class Network 
+{
+public:
+    Network(std::vector<int>&& layers) : layers(layers){
+        for(int layer : layers) {
+            weights.push_back(std::move(Eigen::VectorXd(layer)));
+            for(int i = 0; i < layer; i++) {
+                weights.back()[i] = distribution(generator);
+            }
+        }
+
+        for(auto weight : weights) 
+            std::cout << weight << "\n\n";
+    }
+
+private:
+    std::vector<int> layers;
+    std::vector<Eigen::VectorXd> weights;
+};
 
 int main(int argc, char** argv)
 {
@@ -12,9 +40,10 @@ int main(int argc, char** argv)
                              "A backprop implementation for 'fun'!");
 
     options.add_options() // clang-format off
-        ("b,bar", "Param bar", cxxopts::value<std::string>()) 
-        ("d,debug", "Enable debugging.", cxxopts::value<bool>()->default_value("false")) 
-        ("f,foo", "Param foo.", cxxopts::value<int>()->default_value("10")) 
+        ("b,bar", "Param bar", cxxopts::value<std::string>())
+        ("d,debug", "Enable debugging.", cxxopts::value<bool>()->default_value("false"))
+        ("f,foo", "Param foo.", cxxopts::value<int>()->default_value("10"))
+        ("l,layers", "Layers of the network.", cxxopts::value<std::vector<int>>())
         ("h,help", "Print usage."); // clang-format on
 
     auto result = options.parse(argc, argv);
@@ -27,8 +56,6 @@ int main(int argc, char** argv)
     if (debug)
         console->set_level(spdlog::level::debug);
 
-    Eigen::Matrix<int, 2, 3> a({{1, 2, 3}, {4, 5, 6}});
-    Eigen::Matrix<int, 3, 2> b(a.transpose());
-    console->debug("test log");
-    std::cout << "Hello, world!\n" << a << "\n\n" << b << "\n";
+    std::vector<int> layers = result["layers"].as<std::vector<int>>();
+    std::unique_ptr<Network> nn = std::make_unique<Network>(std::move(layers));
 }
